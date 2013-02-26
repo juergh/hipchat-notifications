8969067e2d54c1b753be0087574fc4a35523eccb
Juerg Haefliger <juerg.haefliger@hp.com>
2013-02-26 08:44:07 +0100

    add a preferences dialog

--- hipchat-notifications.pl
+++ hipchat-notifications.pl
@@ -6,16 +6,18 @@ use Purple;
 %PLUGIN_INFO = (
     perl_api_version => 2,
     name => "HipChat Notifications",
-    version => "0.1",
+    version => "0.2.0",
     summary => "Customized notifications for HipChat rooms",
     description => "",
     author => "Juerg Haefliger <juergh\@gmail.com",
     url => "https://github.com/juergh/hipchat-notifications",
     load => "plugin_load",
-    unload => "plugin_unload"
+    unload => "plugin_unload",
+    prefs_info => "prefs_info_cb"
 );
 
 $PLUGIN_NAME = "hipchat-notifications";
+$PLUGIN_PREFS = "/plugins/core/" . $PLUGIN_NAME . "/";
 
 %CONV_TITLE = ();
 %CONV_COUNT = ();
@@ -23,6 +25,65 @@ $PLUGIN_NAME = "hipchat-notifications";
 
 sub plugin_init {
     return %PLUGIN_INFO;
+}
+
+sub prefs_info_cb {
+    my ($frame, $ppref);
+
+    Purple::Debug::info($PLUGIN_NAME, "prefs_info_cb()\n");
+
+    # Initialize the frame
+    $frame = Purple::PluginPref::Frame->new();
+
+    # Visual notifications
+    $ppref = Purple::PluginPref->new_with_label("Visual Notifications");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "visual-alias", "Color \@<alias> messages");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "visual-alias-color", "Color");
+    $ppref->set_type(2);
+    $ppref->set_max_length(7);
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "visual-all", "Color \@all messages");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "visual-all-color", "Color");
+    $ppref->set_type(2);
+    $ppref->set_max_length(7);
+    $frame->add($ppref);
+
+    # Audible notifications
+    $ppref = Purple::PluginPref->new_with_label("Audible Notifications");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "audible-alias", "Play sound for \@<alias> messages");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "audible-alias-sound", "Sound file");
+    $ppref->set_type(2);
+    $ppref->set_max_length(64);
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "audible-all", "Play sound for \@all messages");
+    $frame->add($ppref);
+
+    $ppref = Purple::PluginPref->new_with_name_and_label($PLUGIN_PREFS .
+        "audible-all-sound", "Sound file");
+    $ppref->set_type(2);
+    $ppref->set_max_length(64);
+    $frame->add($ppref);
+
+    return $frame;
 }
 
 sub enable_notifications_cb {
@@ -69,8 +130,9 @@ sub receiving_chat_msg_cb {
     my ($account, $sender, $message, $conv, $flags, $plugin) = @_;
     my ($all, $nick, $quoted);
     my $name = $conv->get_name();
-    my $notify = 0;
-    my $color = "#000000";
+    my $color = 0;
+    my $sound = 0;
+    my $count = 0;
 
     # Check if this is a HipChat account
     if ($account->get_username() =~ m|chat.hipchat.com/xmpp$|) {
@@ -81,8 +143,19 @@ sub receiving_chat_msg_cb {
 	if ($message =~ /(^|\s)$quoted($|\s)/) {
 	    Purple::Debug::info($PLUGIN_NAME, "message for " . $quoted .
 				" received\n");
-	    $notify = 1;
-	    $color = "#008000";
+
+	    if (Purple::Prefs::get_bool($PLUGIN_PREFS . "visual-all")) {
+		$color = Purple::Prefs::get_string($PLUGIN_PREFS .
+						   "visual-all-color");
+	    }
+
+	    if (Purple::Prefs::get_bool($PLUGIN_PREFS . "audible-all")) {
+		$sound = Purple::Prefs::get_string($PLUGIN_PREFS .
+						   "audible-all-sound");
+	    }
+
+	    $count = 1;
+
 	    goto DONE;
 	}
 
@@ -93,28 +166,44 @@ sub receiving_chat_msg_cb {
 	if ($message =~ /(^|\s)$quoted($|\s)/) {
 	    Purple::Debug::info($PLUGIN_NAME, "message for " . $quoted .
 				" received\n");
-	    $notify = 1;
-	    $color = "#c00000";
+
+	    if (Purple::Prefs::get_bool($PLUGIN_PREFS . "visual-alias")) {
+		$color = Purple::Prefs::get_string($PLUGIN_PREFS .
+						   "visual-alias-color");
+	    }
+
+	    if (Purple::Prefs::get_bool($PLUGIN_PREFS . "audible-alias")) {
+		$sound = Purple::Prefs::get_string($PLUGIN_PREFS .
+						   "audible-alias-sound");
+	    }
+
+	    $count = 1;
+
 	    goto DONE;
 	}
     }
 
 DONE:
-    if ($notify) {
-	# Color the message
+    # Color the message
+    if ($color) {
 	$message = "<font color=" . $color . ">" . $message . "</font>";
+    }
 
-	# Update the conversation title and play a sound if notifications
-	# are enabled and the conversation doesn't have the focus
-	if ($CONV_NOTIFY{$name} && !$conv->has_focus()) {
-	    # Update the conversation title and count
+    # Update the conversation title and play a sound if notifications are
+    # enabled and the conversation doesn't have the focus
+    if ($CONV_NOTIFY{$name} && !$conv->has_focus()) {
+
+	# Update the conversation title and count
+	if ($count) {
 	    $CONV_COUNT{$name}++;
 	    $conv->set_title("(" . $CONV_COUNT{$name}.") " .
 			     $CONV_TITLE{$name});
+	}
 
-	    # Play a sound
-	    system("/usr/bin/canberra-gtk-play -l 1 -V 10.0 " .
-		   "-f /usr/share/sounds/gnome/default/alerts/bark.ogg &");
+	# Play a sound
+	if ($sound) {
+	    system("/usr/bin/canberra-gtk-play -l 1 -V 10.0 -f " . $sound .
+		   " &");
 	}
     }
 
@@ -127,6 +216,23 @@ sub plugin_load {
     my ($conv_handle);
 
     Purple::Debug::info($PLUGIN_NAME, "plugin_load()\n");
+
+    # Initialize the preferences
+    Purple::Prefs::add_none($PLUGIN_PREFS);
+
+    Purple::Prefs::add_bool($PLUGIN_PREFS . "visual-alias", 1);
+    Purple::Prefs::add_string($PLUGIN_PREFS . "visual-alias-color", "#c00000");
+
+    Purple::Prefs::add_bool($PLUGIN_PREFS . "visual-all", 1);
+    Purple::Prefs::add_string($PLUGIN_PREFS . "visual-all-color", "#008000");
+
+    Purple::Prefs::add_bool($PLUGIN_PREFS . "audible-alias", 1);
+    Purple::Prefs::add_string($PLUGIN_PREFS . "audible-alias-sound",
+        "/usr/share/sounds/gnome/default/alerts/bark.ogg");
+
+    Purple::Prefs::add_bool($PLUGIN_PREFS . "audible-all", 1);
+    Purple::Prefs::add_string($PLUGIN_PREFS . "audible-all-sound",
+        "/usr/share/sounds/gnome/default/alerts/bark.ogg");
 
     # Register the Purple callbacks
     $conv_handle = Purple::Conversations::get_handle();
